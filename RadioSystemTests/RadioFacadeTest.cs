2aaeb7a3399543dc88c01b481613567f911e1f39
Kalle <Kalle.hallert@gmail.com>
2015-10-22 10:06:36 +0200

    First commit

---
+++ RadioSystemTests/RadioFacadeTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RadioSystemTests
+{
+    [TestClass]
+    public class RadioFacadeTest
+    {
+        [TestMethod]
+        public void Test_That_Facade_Can_Be_Created()
+        {
+            RadioFacade facade = new RadioFacade();
+            Assert.IsNotNull(facade);
+            //Assert.Fail();
+        }
+
+        //[TestMethod]
+        //[ExpectedException(typeof(NotImplementedException))]
+        //public void Test_That_MixMegapol_Generates_Exeption()
+        //{
+        //    RadioFacade facade = new RadioFacade();
+        //    facade.PlayMusic("MixMegapol");
+        //    //Assert.Fail();
+        //}
+
+        [TestMethod]
+        public void Test_That_RixFM_Can_Be_Played()
+        {
+            RadioFacade facade = new RadioFacade();
+            facade.PlayMusic("RixFM");
+            Assert.IsTrue(facade.IsPlaying);
+            //Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Test_That_P3_Can_Be_Played()
+        {
+            RadioFacade facade = new RadioFacade();
+            facade.PlayMusic("P3");
+            Assert.IsTrue(facade.IsPlaying);
+            //Assert.Fail();
+        }
+    }
+}
